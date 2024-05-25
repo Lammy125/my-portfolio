a7c9cf1114040457641e17c6d79eb62db503a3fa
Adeyemo Peter <45455927+Lammy125@users.noreply.github.com>
2024-05-25 19:02:45 +0100

    changed about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,3 @@
-import { useRef, useState } from "react";
 import "./About.scss";
 import myImage from "../../assets/Adeyemo_Peter-removebg-preview.png";
 
@@ -6,26 +5,8 @@ const About = () => {
   const aboutData = {
     title: "About me",
     paragraphs: [
-      "Hello there!, I'm a passionate frontend developer with a keen eye for detail and a relentless drive for excellence. I possess a unique ability to keenly observe the intricacies of a project, enabling me to understand the requirements and goals thoroughly. By closely examining user interactions and behaviors, I strive to create intuitive and user-friendly interfaces that enhance the overall user experience.",
-      "As a detail-oriented individual, I believe that even the smallest elements can significantly impact the quality of a project. I meticulously scrutinize code, design layouts, and functionalities to ensure precision and accuracy. My dedication to perfection empowers me to deliver projects with clean, efficient, and maintainable code.",
-      "I thrive on perseverance and resilience, always seeking innovative solutions to overcome obstacles. Whether it's debugging a complex issue or refining a feature to meet the highest standards, I approach every challenge with determination and tenacity.",
-      "My expertise lies in frontend development, where I bring life to creative designs and turn them into functional, responsive, and visually appealing websites and applications.",
-      "In summary, I am an observant, detail-oriented, and resilient frontend developer who is committed to creating exceptional digital experiences. I am excited to contribute my skills and expertise to build innovative and impactful projects that leave a positive mark on the digital realm.",
-      "Thank you for taking the time to get to know me. I look forward to collaborating on exciting projects and making a significant difference in the world of software development. Let's build something amazing together!",
+      "Front-End Engineer with over 2 years of experience using ReactJS, NextJS Framework, Tailwind CSS & TypesScript in implementing designs, building scalable applications & optimizing applications for good user experience, co-operating with product designers & managers. I have a strong academic background in Mathematics with Computer Science, which I obtained from the University of Lagos in 2019.",
     ],
-  };
-
-  const [showFullContent, setShowFullContent] = useState(false);
-  const initialParagraphsToShow = 1;
-
-  const paragraphsContainerRef = useRef(null);
-
-  // Function to reset to the default view
-  const resetToDefaultView = () => {
-    setShowFullContent(false);
-    if (paragraphsContainerRef.current) {
-      paragraphsContainerRef.current.scrollTop = 0;
-    }
   };
 
   return (
@@ -36,45 +17,13 @@ const About = () => {
         </div>
         <div className="content">
           <h2 data-aos="fade-down">{aboutData.title}</h2>
-          <div
-            ref={paragraphsContainerRef}
-            className="paragraphs-container"
-            style={{
-              overflow: showFullContent ? "auto" : "hidden",
-              maxHeight: showFullContent ? "320px" : "200px",
-            }}
-            data-aos="fade-right"
-          >
-            {aboutData.paragraphs
-              .slice(
-                0,
-                showFullContent
-                  ? aboutData.paragraphs.length
-                  : initialParagraphsToShow
-              )
-              .map((paragraph, index) => (
-                <p key={index} className="textContent">
-                  {paragraph}
-                </p>
-              ))}
+          <div data-aos="fade-right">
+            {aboutData.paragraphs.map((paragraph, index) => (
+              <p key={index} className="textContent">
+                {paragraph}
+              </p>
+            ))}
           </div>
-          {/* Render the "Read more" button only if there are more paragraphs to show */}
-          {aboutData.paragraphs.length > initialParagraphsToShow && (
-            <div className="btnCon" data-aos="flip-up">
-              {showFullContent ? (
-                <button className="btn" onClick={resetToDefaultView}>
-                  Show less
-                </button>
-              ) : (
-                <button
-                  className="btn"
-                  onClick={() => setShowFullContent(!showFullContent)}
-                >
-                  Read more
-                </button>
-              )}
-            </div>
-          )}
         </div>
       </div>
     </section>
